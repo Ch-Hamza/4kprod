acf8c56919f716ca0d5e364cd9651e78ce65f77b
ch-hamza <hamzachebil40@gmail.com>
2020-08-16 01:36:22 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {MainComponent} from './main/main.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainComponent },
+  { path: '**', component: MainComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { anchorScrolling: 'enabled'})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MainComponent } from './main/main.component';
+import { FooterComponent } from './footer/footer.component';
+import { LandingComponent } from './sections/landing/landing.component';
+import { AboutComponent } from './sections/about/about.component';
+import { ServicesComponent } from './sections/services/services.component';
+import { PortfolioComponent } from './sections/portfolio/portfolio.component';
+import { TeamComponent } from './sections/team/team.component';
+import { PartnersComponent } from './sections/partners/partners.component';
+import { ContactComponent } from './sections/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MainComponent,
+    FooterComponent,
+    LandingComponent,
+    AboutComponent,
+    ServicesComponent,
+    PortfolioComponent,
+    TeamComponent,
+    PartnersComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
